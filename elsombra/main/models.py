7174a8a105e35f20199c625a31e2a8b503f3a5f2
Bantu-art <brianbaraza40@gmail.com>
2025-04-24 17:40:46 +0300

    Add UserProfile model with user details and timestamps

--- elsombra/main/models.py
+++ elsombra/main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class UserProfile(model.Models):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=15, blank=True)
+    address = models.TextField(blank=True)
+    city = models.CharField(max_length=100, blank=True)
+    postal_code = models.CharField(max_length=20, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.user.username
